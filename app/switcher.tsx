2c3c9c0b3c4d22daf11a079cd9bfce0f7244d8bf
Oscar Leal <oscarleal234@gmail.com>
2024-09-13 15:42:44 -0500

    setup themes and NEXTUI provider

---
+++ app/switcher.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import { Switch } from '@nextui-org/react';
+import { FaSun, FaMoon } from 'react-icons/fa6';
+
+const Switcher = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  function handleTheme() {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }
+  return (
+    <div>
+      <Switch
+        size="sm"
+        defaultSelected
+        color="success"
+        onClick={() => handleTheme()}
+        thumbIcon={({ isSelected, className }) =>
+          isSelected ? <FaSun className="text-black" /> : <FaMoon />
+        }
+      ></Switch>
+    </div>
+  );
+};
+
+export default Switcher;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/react';
+import { addDynamicIconSelectors } from '@iconify/tailwind';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [
+    require('daisyui'),
+    addDynamicIconSelectors(),
+    nextui({
+      themes: {
+        light: {
+          layout: {},
+          colors: {
+            foreground: '#000000',
+          },
+        },
+        dark: {
+          layout: {},
+          colors: {
+            foreground: '#ffffff',
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
